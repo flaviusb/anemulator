821329b4b825b75d8d9337e864f9c7dccc4bc553
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2018-08-10 23:57:19 +1200

    Change encoding for instructions around, and make masks actually mask.

--- src/instruction/mod.rs
+++ src/instruction/mod.rs
@@ -1,5 +1,6 @@
 macro_rules! instructions {
   (enum $enum_raw:ident; enum $enum_processed:ident; fn $fn_raw:ident; fn $fn_processed:ident; cell $cell:ty, $cell_var:ident; $(let $anvar:ident = $anexpr:expr);+ ; @match $extract:expr; @instructions $($ins:ident = $encoding:expr, $($id:ident|$size:ty),*);+;) => (
+      #[derive(Eq, PartialEq, Debug)]
       enum $enum_raw {$(
         $ins( $( $size ),* )
       ),+}
@@ -19,11 +20,11 @@ type d13 = usize;
 type v13 = u16;
 type Cell = u64;
 
-const I0: u64 = 0b111111111111_0000000000000_0000000000000_0000000000000_0000000000000;
-const I1: u64 = 0b000000000000_1111111111111_0000000000000_0000000000000_0000000000000;
-const I2: u64 = 0b000000000000_0000000000000_1111111111111_0000000000000_0000000000000;
-const I3: u64 = 0b000000000000_0000000000000_0000000000000_1111111111111_0000000000000;
-const I4: u64 = 0b000000000000_0000000000000_0000000000000_0000000000000_1111111111111;
+const I0: u64 = 0b0000000000000_0000000000000_0000000000000_0000000000000_111111111111;
+const I1: u64 = 0b0000000000000_0000000000000_0000000000000_1111111111111_000000000000;
+const I2: u64 = 0b0000000000000_0000000000000_1111111111111_0000000000000_000000000000;
+const I3: u64 = 0b0000000000000_1111111111111_0000000000000_0000000000000_000000000000;
+const I4: u64 = 0b1111111111111_0000000000000_0000000000000_0000000000000_000000000000;
 
 instructions! {
   enum InstructionsRaw;
@@ -31,14 +32,22 @@ instructions! {
   fn matcher_raw;
   fn matcher_processed;
   cell Cell, n;
-  let extract = (n | I0) >> 52;
-  let r1      = (n | I1) >> 39;
-  let r2      = (n | I2) >> 26;
-  let r3      = (n | I3) >> 13;
-  let r4      = (n | I4);
+  let extract = (n & I0);
+  let r1      = (n & I1) >> 12;
+  let r2      = (n & I2) >> 25;
+  let r3      = (n & I3) >> 38;
+  let r4      = (n & I4) >> 51;
   @match extract;
   @instructions
   Name = 0x01, r1|a13, r2|a13, r3|a13, r4|d13;
   Mame = 0x02, r1|a13, r2|a13;
   Fame = 0x03, r1|v13, r2|a13;
+}
+
+#[test]
+fn test_instruction_parsing() {
+  let m: u64 = 0b0000000000011_0000000000000_0000000000000_0000000000000_000000000001;
+  let n: u64 = 0b0000000000000_0000000000000_0000000000000_1000000000000_000000000010;
+  assert_eq!(matcher_raw(m), InstructionsRaw::Name(0, 0, 0, 3));
+  assert_eq!(matcher_raw(n), InstructionsRaw::Mame(0b1000000000000, 0));
 }
