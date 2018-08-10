1a4a1f38e73a440cdb37b2748c0d48bd5020a7ab
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2018-08-10 21:23:31 +1200

    Add cell type, extract expression in instruction macro.

--- src/instruction/mod.rs
+++ src/instruction/mod.rs
@@ -1,5 +1,5 @@
 macro_rules! instructions {
-  (enum $enum:ident; fn $fn:ident; @instructions $($ins:ident = $encoding:expr, $($size:ty),*);+) => (
+  (enum $enum:ident; fn $fn:ident; cell $cell:ty, $cell_var:ident; extract $extract:expr; @instructions $($ins:ident = $encoding:expr, $($size:ty),*);+) => (
       enum $enum {$(
         $ins( $( $size ),* )
       ),+}
@@ -8,10 +8,13 @@ macro_rules! instructions {
 
 type a13 = u32;
 type d13 = u32;
+type Cell = u64;
 
 instructions! {
   enum Instructions;
   fn match;
+  cell Cell, n;
+  extract (n || 0b111111111111);
   @instructions
   Name = 0x01, a13, a13, a13, d13;
   Mame = 0x02, a13, a13
