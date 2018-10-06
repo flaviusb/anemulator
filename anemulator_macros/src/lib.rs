6d8e9694324b17e1a445fa9858735feaead90238
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2018-10-06 20:20:00 +1300

    Use full path to TokenStream.

--- anemulator_macros/src/lib.rs
+++ anemulator_macros/src/lib.rs
@@ -1,11 +1,11 @@
 #![feature(proc_macro_diagnostic)]
-#![feature(proc_macro_non_items)]
+#![feature(proc_macro_hygiene)]
 #![feature(proc_macro_quote)]
 #![feature(proc_macro_span)]
 
 extern crate proc_macro;
 
 #[proc_macro]
-pub fn define_chip(input: TokenStream) -> TokenStream {
+pub fn define_chip(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
   panic!("Not implemented.");
 }
