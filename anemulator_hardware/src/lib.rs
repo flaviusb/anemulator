1aee51f9209b8a3ac02be3306de71f69a4269142
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2018-10-06 21:05:55 +1300

    Add stub for anemulator_hardware module.

---
+++ anemulator_hardware/src/lib.rs
@@ -0,0 +1,14 @@
+#![feature(non_ascii_idents)]
+
+extern crate tokio;
+extern crate futures;
+
+#[macro_use]
+extern crate anemulator_macros;
+
+use tokio::prelude::*;
+use tokio::prelude::Future;
+
+mod potato;
+
+
